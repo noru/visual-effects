8b3959f54bfd29776ad43020ff20a2aba3860d2b
drew <nooooru@gmail.com>
2019-08-28 23:02:54 +0800

    fireworks: final touch

--- src/fireworks.ts
+++ src/fireworks.ts
@@ -1,20 +1,31 @@
 import { TAU, random, sample, randomInt } from '@drewxiu/utils'
 import { Colors } from './utils/common'
 
-class Firework {
+interface Particle {
+  x: number
+  y: number
+  speed: number
+  opacity: number
+  opacityStep: number
+  yV: number
+  rotation: number
+}
+
+class Shot {
   x!: number
+  driftedX!: number
   height!: number
   explodeHeight!: number
   initSpeed!: number
   color!: string
   radius!: number
   distanceFromCameraFactor!: number
-  particles!: any[]
+  particles!: Particle[]
   context: CanvasRenderingContext2D
   finished = false
 
   gravity = 0.05
-  friction = 0.9
+  friction = 0.96
 
   constructor(context: CanvasRenderingContext2D) {
     this.context = context
@@ -31,15 +42,16 @@ class Firework {
   }
 
   next() {
-    if (this.height < this.explodeHeight) {
-      this.height += this.initSpeed *= 0.97
+    if (this.height < this.explodeHeight ) {
+      this.height += this.initSpeed *= 0.96
+      this.x = (this.driftedX - this.x) * 0.035 + this.x
     } else {
       let count = 0
       this.particles.forEach(p => {
         p.x += p.speed * Math.cos((p.rotation * Math.PI) / 180)
         p.y += p.speed * Math.sin((p.rotation * Math.PI) / 180) + (p.yV += this.gravity)
         p.speed *= this.friction
-        p.opacity -= 0.02
+        p.opacity -= p.opacityStep
         if (p.opacity <= 0) {
           p.opacity = 0
           count++
@@ -71,7 +83,7 @@ class Firework {
     this.particles.forEach(p => {
       ctx.save()
       ctx.beginPath()
-      ctx.arc(p.x, p.y, 5, 0, TAU)
+      ctx.arc(p.x, p.y, 3, 0, TAU)
       ctx.fillStyle = this.color
       ctx.globalAlpha = p.opacity
       ctx.fill()
@@ -82,31 +94,32 @@ class Firework {
   reset() {
     let canvas = this.context.canvas
     this.height = 0
-    this.distanceFromCameraFactor = random(0.5, 1)
+    this.distanceFromCameraFactor = random(0.3, 1)
     this.explodeHeight = this.distanceFromCameraFactor * 0.9 * this.context.canvas.height
     this.x = random(canvas.width * 0.1, canvas.width * 0.9)
+    this.driftedX = random(-2, 2) * canvas.width / 30 + this.x
     this.color = sample(Colors)
     this.radius = this.explodeHeight * 0.3
     this.initSpeed = this.explodeHeight * 0.05
     this.particles = Array.from({ length: 150 }, _ => ({
-      x: this.x,
+      x: this.driftedX,
       y: canvas.height - this.explodeHeight,
       yV: 0,
       rotation: randomInt(0, 360),
-      speed: (random(3, 6) * this.radius) / 60,
-      opacity: random(0.5, 1),
+      speed: random(.03, .06) * this.radius,
+      opacity: this.distanceFromCameraFactor,
+      opacityStep: 0.01 * this.distanceFromCameraFactor,
     }))
   }
 }
 
 interface Config {
   container?: HTMLElement
-  colors?: string[]
   width?: number
   height?: number
 }
 
-export function fireworks({ container = document.body, colors = Colors, width, height }: Config = {}) {
+export function fireworks({ container = document.body, width, height }: Config = {}) {
   let frameId
   let ratio = window.devicePixelRatio
   width = width || container.offsetWidth
@@ -122,19 +135,19 @@ export function fireworks({ container = document.body, colors = Colors, width, h
   context.fillStyle = 'black'
   context.fillRect(0, 0, canvas.width, canvas.height)
 
-  let fireworks = new Array<Firework>()
+  let shots = new Array<Shot>()
 
   let lastFire = 0
   function render() {
     context.fillStyle = 'rgba(0, 0, 0, 0.05)'
     context.fillRect(0, 0, canvas.width, canvas.height)
     let now = Date.now()
-    if (now - lastFire > random(500, 1000)) {
+    if (now - lastFire > random(100, 400)) {
       lastFire = now
-      fireworks.push(new Firework(context))
+      shots.push(new Shot(context))
     }
-    fireworks = fireworks.filter(f => !f.finished)
-    fireworks.forEach(f => f.draw())
+    shots = shots.filter(f => !f.finished)
+    shots.forEach(f => f.draw())
     frameId = requestAnimationFrame(render)
   }
 
